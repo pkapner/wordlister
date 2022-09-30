4cc0e3a786e14de990a77b62168ae8c2dd0be3dc
Peter Kapner <peter@peterkapner.com>
2022-09-30 17:49:14 -0400

    Added custom Comparitor to sort by length and alpha

--- WordListerApp/src/main/java/com/uestechnology/WordLister.java
+++ WordListerApp/src/main/java/com/uestechnology/WordLister.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -177,8 +180,10 @@ public class WordLister {
             default:
                 curList = longWords;
         }
+        List<String> printableList = new ArrayList<>(curList);
+        printableList.sort(new LengthFirstComparator());
 
-        for (String word : curList) {
+        for (String word : printableList) {
             if (shortWords.contains(word) && type == SetType.MEDIUM) {
                 System.out.print(ANSI_RESET + ANSI_GREEN);
             } else if (shortWords.contains(word) && type == SetType.LONG) {
@@ -208,6 +213,15 @@ public class WordLister {
         displayWords(shortWords, medWords, longWords, SetType.LONG);
     }
 
+    public static class LengthFirstComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1.length() != o2.length()) {
+                return o1.length() - o2.length(); //overflow impossible since lengths are non-negative
+            }
+            return o1.compareTo(o2);
+        }
+    }
 
     private void setDebug() {
         this.debug = true;
